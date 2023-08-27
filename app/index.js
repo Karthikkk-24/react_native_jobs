2ac5fdb7f1b25c45436428dbf2b851bb673cc239
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2023-08-28 00:14:59 +0530

    Added included files & file structure

--- app/index.js
+++ app/index.js
@@ -1,4 +1,9 @@
-import { Text, View } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+import { useState } from 'react';
+import { SafeAreaView, ScrollView, Text, View } from 'react-native';
+
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
+import { COLORS, icons, images, SIZES } from '../constants';
 
 const Home = () => {
     return (
